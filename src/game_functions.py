c5a7a54ae25065e3dc962d37437478645b27795d
nicklamiller <nicklamiller@gmail.com>
2023-01-28 21:47:56 -0800

    add left movement functionality

--- src/game_functions.py
+++ src/game_functions.py
@@ -20,10 +20,14 @@ def check_events(ship: Ship) -> None:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(
