e0f48e4ab305d8b3a1df8b6b797cfc11250d50dc
nicklamiller <nicklamiller@gmail.com>
2023-03-26 17:50:51 -0700

    add aliens to game functions

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -3,12 +3,11 @@ import pygame
 from pygame.sprite import Group
 
 from src import game_functions as gf
-from src.alien import Alien
 from src.settings import Settings
 from src.ship import Ship
 
 
-def run_game():  # noqa: WPS213
+def run_game():  # noqa: WPS210, WPS213
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode(
@@ -20,14 +19,16 @@ def run_game():  # noqa: WPS213
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(game_settings, screen)
     bullets = Group()
-    alien = Alien(game_settings, screen)
+    aliens = Group()
+
+    gf.create_fleet(game_settings, screen, aliens)
 
     while True:
 
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(game_settings, screen, ship, alien, bullets)
+        gf.update_screen(game_settings, screen, ship, aliens, bullets)
         screen.fill(game_settings.background_color)
         ship.blitme()
 
